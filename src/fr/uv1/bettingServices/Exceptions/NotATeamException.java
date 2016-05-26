0017918e265d3142aebf57b4efb4306ec585db6e
airhboul <anas.irhboula@telecom-bretagne.eu>
2016-05-26 02:51:40 +0200

    done

---
+++ src/fr/uv1/bettingServices/Exceptions/NotATeamException.java
@@ -0,0 +1,15 @@
+package fr.uv1.bettingServices.Exceptions;
+
+public class NotATeamException extends Exception {
+
+	public NotATeamException() {
+		//System.out.println(string);
+		super();
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
